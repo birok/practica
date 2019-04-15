ca9a56fb049064ebcb4fe92a6032795f32d6e049
Ulises Tlatoani <birok@la-MacBook-Pro-de-Ulises.local>
2019-04-15 11:56:50 -0500

    Primera versión de la practica

---
+++ __manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Curso rNet",
+
+    'summary': """
+        Administración de cursos""",
+
+    'description': """
+        Módulo de rNet para administrar los cursos:
+            - Gestión de cursos
+            - Gestión de sesiones
+            - Registro de participantes
+    """,
+
+    'author': "Ulises Vidal",
+    'website': "http://www.rnet.mx",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'demo/demo.xml',
+        'views/curso_rnet.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo/demo.xml',
+    ],
+}
